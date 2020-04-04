5a97ffe1ddc583880eccfa44e2641ce3e7a3ba41
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-04-04 17:53:22 -0500

    basic login using passport

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 require('dotenv').config(); // For hiding the Coronavirus Monitor API key
 
 // Sets up the Express App
@@ -18,10 +21,16 @@ app.use(express.json());
 // Static directory
 app.use(express.static("public"));
 
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 // =============================================================
 require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app)
+require("./routes/html-routes.js")(app);
+
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync().then(function () {
