0f0d8d3f5329073be3cb7e6dd6880bf980a35e1a
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-04-09 00:53:35 -0500

    fixing typo in api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   
     // POST route for saving a new closetohome. We can create closehome with the data in req.body -- tested
     app.post("/api/closetohome", function(req, res) {
-      var newFamily=req.body
+      var newFamily=req.body;
 
       db.Family.create(newFamily).then(response=>{
 
@@ -32,7 +32,7 @@ module.exports = function(app) {
           id:  req.params.id
         }
       }).then(response=>{
-        res.json(response)
+        res.json(response);
       })
     });
   
@@ -132,46 +132,43 @@ module.exports = function(app) {
     
       });
           // GET route for getting all of the closetohome
-    app.get("/api/sympton", function(req, res) {
-        db.Sympton.findAll({}).then(response=>{
-          res.json(response)
-        })
-      });
+  app.get("/api/sympton", function(req, res) {
+    db.Sympton.findAll({}).then(response=>{
+      res.json(response)
+    })
+  });
     
-      // POST route for saving a new closetohome. We can create todo with the data in req.body
-      app.post("/api/sympton", function(req, res) {
-        var newSympton=req.body
-        db.Sympton.create(newSympton).then(response=>{
-          res.json(response)
-        })
-      });
+  // POST route for saving a new closetohome. We can create todo with the data in req.body
+  app.post("/api/sympton", function(req, res) {
+    var newSympton=req.body
+    db.Sympton.create(newSympton).then(response=>{
+      res.json(response)
+    })
+  });
     
-      // DELETE route for deleting closetohome. We can get the id of the todo to be deleted from
-      // req.params.id
-      app.delete("/api/sympton/:id", function(req, res) {
-        db.Sympton.destroy({
-          where:{
-            id:  req.params.id
-          }
-        }).then(response=>{
-          res.json(response)
-        })
-      });
-    
-      // PUT route for updating closetohome. We can get the updated todo data from req.body
-      app.put("/api/closetohome/:id", function(req, res) {
-        var newSympton = req.body
-        db.Sympton.update(newSympton,{
-          where:{
-            id: req.params.id
-          }
-        }).then(response=>{
-          res.json(response)
-        })
-    
-      });
-  };
+  // DELETE route for deleting closetohome. We can get the id of the todo to be deleted from
+  // req.params.id
+  app.delete("/api/sympton/:id", function(req, res) {
+    db.Sympton.destroy({
+      where:{
+        id:  req.params.id
+      }
+    }).then(response=>{
+      res.json(response)
+    })
+  });
 
+  // PUT route for updating closetohome. We can get the updated todo data from req.body
+  app.put("/api/closetohome/:id", function(req, res) {
+    var newSympton = req.body
+    db.Sympton.update(newSympton,{
+      where:{
+        id: req.params.id
+      }
+    }).then(response=>{
+      res.json(response)
+    })
+  });
 
   // Route for getting information about a given individual
   // For now, just looks up the number of cases for the user's state and returns that
